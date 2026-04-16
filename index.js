243f807e8184084bf3dcb982ed387a111e07b3d8
rahim-dad <rahimdadwahi@gmail.com>
2026-04-16 15:34:02 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+// Primitive Data Types
+
+let name = "Rahim";        // String
+let age = 25;              // Number
+let isStudent = true;      // Boolean
+let data;                  // Undefined
+let empty = null;          // Null
+let bigNum = 123456789n;   // BigInt
+let id = Symbol("id");     // Symbol
+
+console.log(name, age, isStudent, data, empty, bigNum, id);
