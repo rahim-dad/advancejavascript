ee8a78f364e549e2a24368104c2105ec4d60ad24
rahim-dad <rahimdadwahi@gmail.com>
2026-04-16 15:38:37 +0500

    first commit

---
+++ non_permatives.js
@@ -0,0 +1,19 @@
+// Non-Primitive Data Types
+
+// Object
+let person = {
+  name: "Rahim",
+  age: 25
+};
+
+// Array
+let numbers = [1, 2, 3, 4];
+
+// Function
+function greet() {
+  console.log("Hello");
+}
+
+console.log(person);
+console.log(numbers);
+greet();
